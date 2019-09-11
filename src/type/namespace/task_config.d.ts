594256f3749d58488ed078e9d13a05897a14b1ea
yaozeyuan <yaozeyan93@gmail.com>
2019-09-12 00:23:00 +0800

    feat: 添加任务配置demo

--- src/type/namespace/task_config.d.ts
+++ src/type/namespace/task_config.d.ts
@@ -1,23 +1,8 @@
 declare namespace TaskConfig {
-  type AuthorCollectionType =
-    | 'author-ask-question'
-    | 'author-answer'
-    | 'author-pin'
-    | 'author-agree-answer'
-    | 'author-agree-article'
-    | 'author-watch-question'
-  type ItemCollectionType = 'topic' | 'collection' | 'column' | 'article' | 'question' | 'answer' | 'pin'
-  type orderBy = 'createAt' | 'updateAt' | 'voteUpCount' | 'commentCount'
-  type order = 'asc' | 'desc'
-  type orderByList = Array<{
-    orderBy: orderBy
-    order: order
-  }>
   type imageQuilty = 'default' | 'none' | 'raw' | 'hd'
-  type maxQuestionOrArticleInBook = number // 自动分卷: 单本电子书中最大问题/文章数量
+  type maxBlogInBook = number // 自动分卷: 单本电子书中最大问题/文章数量
   type Record = {
-    type: ItemCollectionType | AuthorCollectionType
-    id: string | number
+    uid: string
     rawInputText: string
     comment: string // 备注
   }
@@ -28,8 +13,7 @@ declare namespace TaskConfig {
     imageQuilty: imageQuilty // 图片质量
     bookTitle: string // 书名
     comment: string // 备注
-    maxQuestionOrArticleInBook: maxQuestionOrArticleInBook // 自动分卷: 单本电子书中最大问题/文章数量
-    orderByList: orderByList
+    maxBlogInBook: maxBlogInBook // 自动分卷: 单本电子书中最大微博数
   }
 }
 
