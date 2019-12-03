0662e13a96e4dc48701f7b2cdc38fc462735a7ef
yaozeyuan <yaozeyuan93@gmail.com>
2019-12-04 01:39:35 +0800

    test

---
+++ precache-manifest.bb4ec016dbca625b601f602196135fbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df9c325cb91348cf7230bc017bc88bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16753850eab908c3b8d1",
+    "url": "./static/css/main.fb5ac955.chunk.css"
+  },
+  {
+    "revision": "48d83761d3c2d6653bb7",
+    "url": "./static/js/2.b60093c4.chunk.js"
+  },
+  {
+    "revision": "16753850eab908c3b8d1",
+    "url": "./static/js/main.23f809c9.chunk.js"
+  },
+  {
+    "revision": "5c2294f02aac60a940dd",
+    "url": "./static/js/runtime-main.ab423ae6.js"
+  },
+  {
+    "revision": "7ca102d46819c8148d73d30a7e9cd081",
+    "url": "./static/media/mac.7ca102d4.svg"
+  },
+  {
+    "revision": "899da5459c71c0f150139e54f7007eea",
+    "url": "./static/media/stablog_logo_256x256.899da545.png"
+  },
+  {
+    "revision": "185acfd1d2a12ac647accc71d64f7ac9",
+    "url": "./static/media/windows.185acfd1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ab42e9c541ff279d2ec8ad1ecbf789a.js"
+  "./precache-manifest.bb4ec016dbca625b601f602196135fbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
