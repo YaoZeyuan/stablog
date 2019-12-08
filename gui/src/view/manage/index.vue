fcb4ec8d5561c4ed4b3e0ca8c4e898d6c475fda1
yaozeyuan <yaozeyan93@gmail.com>
2019-12-08 17:49:33 +0800

    feat: 初始化数据管理界面

---
+++ gui/src/view/manage/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      管理后台
+      </div>
+  </div>
+</template>
+
+<script>
+  import _ from "lodash"
+
+  export default {
+    name: 'manage',
+    data(){
+        return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
