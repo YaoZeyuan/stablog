091e1a1b19aa921facd962698b53155bc4e8540d
yaozeyuan <yaozeyuan93@gmail.com>
2021-02-10 22:46:51 +0800

    feat: 尝试用react重写界面, 使用umi构建

---
+++ v2_gui/.umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  fastRefresh: {},
+});
