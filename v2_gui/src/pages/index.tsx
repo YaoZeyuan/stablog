20947422a67c00558acc848d6013da4730c9e131
yaozeyuan <yaozeyuan93@gmail.com>
2021-02-15 16:17:30 +0800

    feat: 不使用路由, 直接使用tab切换页面

--- v2_gui/src/pages/index.tsx
+++ v2_gui/src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { Tabs } from 'antd';
 import './index.less';
+import CustomerTask from './customer_task';
+import DataManage from './manage';
+import Helper from './helper';
+import Log from './log';
+import Login from './login';
 
 const { TabPane } = Tabs;
 
@@ -7,19 +12,19 @@ export default function IndexPage() {
   return (
     <Tabs defaultActiveKey="1" centered>
       <TabPane tab="系统设置" key="1">
-        Content of Tab Pane 1
+        <CustomerTask></CustomerTask>
       </TabPane>
       <TabPane tab="管理数据" key="2">
-        Content of Tab Pane 2
+        <DataManage></DataManage>
       </TabPane>
       <TabPane tab="运行日志" key="3">
-        Content of Tab Pane 3
+        <Log></Log>
       </TabPane>
       <TabPane tab="登录微博" key="4">
-        Content of Tab Pane 3
+        <Login></Login>
       </TabPane>
       <TabPane tab="使用说明" key="5">
-        Content of Tab Pane 3
+        <Helper></Helper>
       </TabPane>
     </Tabs>
   );
