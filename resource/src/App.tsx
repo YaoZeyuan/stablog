1ecbeedaf1a561b1430b565d148007b7ed126389
yaozeyuan001 <yaozeyuan001@ke.com>
2022-08-01 21:36:56 +0800

    feat: 调整项目说明

--- resource/src/App.tsx
+++ resource/src/App.tsx
@@ -61,13 +61,19 @@ export default class Base extends Component {
           <div className="container">
             <div className="title">稳部落</div>
             <div className="slogan">专业备份导出微博记录</div>
-            <div className="desc">备份原理: </div>
+            <div className="version">---项目原理---</div>
+            <div className="desc">软件本身就是一个内置了微博的移动版页面的浏览器</div>
             <div className="desc">
-              登录&nbsp;
+              在软件内登录&nbsp;
               <a href="https://m.weibo.cn" target="_blank">
                 m.weibo.cn
               </a>
-              &nbsp;后, 模拟浏览器访问, 获取登录用户发布的所有微博并备份之
+              &nbsp;后
+            </div>
+            <div className="desc">软件模拟浏览器, 自动浏览登录用户发布的所有微博, 并记录到数据库里</div>
+            <div className="desc">访问完毕后将每条微博截屏成图片, 码在pdf中, 即为备份.</div>
+            <div className="desc">
+              (为方便查找, 在每个图片前加上了微博的文字版, 这样pdf里一搜就能搜到对应微博所在位置)
             </div>
             <div className="desc">
               所以, 即使炸号, 只要登录&nbsp;
@@ -76,10 +82,17 @@ export default class Base extends Component {
               </a>
               &nbsp; 后还能看见自己的微博,就可以备份
             </div>
+            <div className="desc">
+              (反过来, 如果
+              <a href="https://m.weibo.cn" target="_blank">
+                m.weibo.cn
+              </a>
+              都登录不了, 就没办法了)
+            </div>
             <div className="desc"></div>
-            <div className="desc">最新版本：v{`${config.version}`.padEnd(3, '.0')}</div>
+            <div className="version">---最新版本：v{`${config.version}`.padEnd(3, '.0')}---</div>
             <div className="logo">
-              <img src={logo} />
+              <img src="https://tva1.sinaimg.cn/large/007Yq4pTly1h4rkzfkfx5j3074074ab5.jpg" alt="logo" />
             </div>
             <div className="download-container">
               <div className="download-tip">下载</div>
