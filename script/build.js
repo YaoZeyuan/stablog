3e25f79b55288cce7a28c8488db55a70818f3330
yaozeyuan <yaozeyuan93@gmail.com>
2022-08-01 22:29:17 +0800

    feat: 删除所有js.map文件, 以减少体积

--- script/build.js
+++ script/build.js
@@ -1,5 +1,7 @@
 let shelljs = require('shelljs')
 let path = require('path')
+let fs = require('fs')
+let sys = require('sys')
 
 const Const_Root_Path = path.resolve(__dirname, '..')
 // const Const_App_Path = path.resolve(Const_Root_Path, 'app')
@@ -7,6 +9,39 @@ const Const_Root_Path = path.resolve(__dirname, '..')
 const Const_Dist_Path = path.resolve(Const_Root_Path, 'dist')
 const Const_Client_Path = path.resolve(Const_Root_Path, 'client')
 const Const_Client_Dist_Path = path.resolve(Const_Client_Path, 'dist')
+// sqlite3的构建文件, 没有实际用处, 17mb
+const Const_NodeModules_Path = path.resolve(Const_Root_Path, 'node_modules')
+const Const_NodeModules_Sqlite3_Cache_Path = path.resolve(Const_NodeModules_Path, 'sqlite3/build')
+const Const_NodeModules_JsPdf_Dist_Path = path.resolve(Const_NodeModules_Path, 'jspdf/dist')
+
+function getAllJsMapUri(basePath) {
+  let pathUri = path.resolve(basePath)
+  let filenameList = fs.readdirSync(pathUri)
+  let jsMapUriSet = new Set()
+  let currentDirList = [pathUri]
+  let nextDirList = []
+  while (currentDirList.length > 0) {
+    for (currentPath of currentDirList) {
+      for (let filename of filenameList) {
+        let uri = path.resolve(currentPath, filename)
+        let fsStat = fs.statSync(uri)
+        if (fsStat.isDirectory()) {
+          nextDirList.push(uri)
+          continue
+        }
+        if (fsStat.isFile()) {
+          if (filename.endsWith('.js.map')) {
+            jsMapUriSet.add(uri)
+          }
+        }
+      }
+    }
+    currentDirList = nextDirList
+    nextDirList = []
+  }
+  // 得到所有js.map文件的地址
+  return [...jsMapUriSet.values()]
+}
 
 // 清除旧目录资源
 console.log('0. clear')
@@ -32,6 +67,17 @@ console.log('3. copy new static`')
 shelljs.mkdir('-p', './dist/client/dist')
 console.log('move resource')
 shelljs.cp('-rf', './client/dist/*', './dist/client/dist/')
+
+// 减少体积, 删除构建文件
+shelljs.rm('-rf', Const_NodeModules_Sqlite3_Cache_Path)
+// 递归删除所有.js.map文件
+console.log('remove all js.map')
+let allJsMapUriList = getAllJsMapUri(Const_NodeModules_Path)
+console.log(`all js.map count => ${allJsMapUriList.length}`)
+for (let uri of allJsMapUriList) {
+  shelljs.rm('-rf', uri)
+}
+
 // 将dist目录迁移到app目录中, 只发布需要的部分
 // shelljs.mkdir('-p', Const_App_Dist_Path)
 // shelljs.cp('-rf', './dist/*', Const_App_Dist_Path)
