a33c4d95762ef0778f2ddbaf3dfac828b9f958ee
yaozeyuan001 <yaozeyuan001@ke.com>
2022-11-14 23:45:35 +0800

    feat: 替换publicPath

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ASSET_URL = process.env.ASSET_URL || '.'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '.',
+  base: ASSET_URL,
   server: {
     host: 'localhost',
   },
