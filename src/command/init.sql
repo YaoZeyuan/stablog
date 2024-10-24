583397575f02502197c063f36b28b4bcb47527e1
yangmingming <yunmengze210@163.com>
2024-10-24 13:31:31 +0800

    feat: 添加抓取失败记录

--- src/command/init.sql
+++ src/command/init.sql
@@ -8,6 +8,19 @@ CREATE TABLE  IF NOT EXISTS `total_mblog` (
   PRIMARY KEY (`id`)
 ); 
 
+CREATE TABLE  IF NOT EXISTS `fetch_error_record` (
+  `id` INTEGER AUTOINCREMENT, ---- COMMENT '记录id',
+  `author_uid` varchar(30) NOT NULL DEFAULT '', ---- COMMENT '博主uid',
+  `type` varchar(30) NOT NULL DEFAULT 'page', ---- COMMENT '错误类型: page=>页面抓取失败, long_text_weibo => 长微博抓取失败, ',
+  `long_text_weibo_id` varchar(30) NOT NULL DEFAULT '' ---- COMMENT '抓取失败的长微博id',
+  `article_url` varchar(1000) NOT NULL DEFAULT '' ---- COMMENT '抓取失败的微博文章url',
+  `lastest_page_mid` varchar(30) NOT NULL DEFAULT '', ---- COMMENT '上一页成功抓取的微博id, 重抓时, 使用微博id进行抓取',
+  `lastest_page_offset` int(10) NOT NULL DEFAULT 1, ---- COMMENT '距离最近成功抓取微博的页码, 重抓时, 通过该页面确认应向后抓取多少页, 默认只抓取一页',
+  `error_info_json` text, --- COMMENT '报错内容json, 用于调试'
+  `debug_info` text, --- COMMENT '当时运行配置, 用于调试'
+  PRIMARY KEY (`id`)
+); 
+
 CREATE TABLE  IF NOT EXISTS `total_user` (
   `author_uid` varchar(30) NOT NULL DEFAULT '', ---- COMMENT '博主uid',
   `raw_json` text, --- COMMENT '原始响应json'
