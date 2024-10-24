1af14d0a188c1b6495d9dc59e8390f4ed05efe5f
yangmingming <yunmengze210@163.com>
2024-10-24 20:33:44 +0800

    feat: 更新数据库版本号

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 class Database {
-  static readonly version = '1.0.0'
+  static readonly version = '1.1.0'
   static readonly uri: string = path.resolve(__dirname, `../../mblog_v${Database.version}.sqlite`)
 }
 export default Database
