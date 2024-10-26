abe18ba26125caeab4a6757739ad76fd157a6248
yangmingming <yunmengze210@163.com>
2024-10-27 00:44:26 +0800

    feat: sqlite3 依赖调整为 better-sqlite3

--- src/library/knex.ts
+++ src/library/knex.ts
@@ -3,7 +3,7 @@ import Database from '~/src/config/database'
 /**  knex 方式 */
 
 const Knex = knex({
-  client: 'sqlite3',
+  client: 'better-sqlite3',
   connection: {
     filename: Database.uri
   },
@@ -11,12 +11,12 @@ const Knex = knex({
   pool: {
     max: 1, // 不能开多线程去访问同一个sqllite实例, 否则会报SQLITE_BUSY错误
     min: 0,
-        // 由于存在资源池, 导致句柄不被释放, 程序不能退出
-        // 因此将最小句柄数设为0, 每100ms检查一次是否有超过120ms未被使用的资源
-        // 以便句柄的及时回收
-        // free resouces are destroyed after this many milliseconds
+    // 由于存在资源池, 导致句柄不被释放, 程序不能退出
+    // 因此将最小句柄数设为0, 每100ms检查一次是否有超过120ms未被使用的资源
+    // 以便句柄的及时回收
+    // free resouces are destroyed after this many milliseconds
     idleTimeoutMillis: 100,
-        // how often to check for idle resources to destroy
+    // how often to check for idle resources to destroy
     reapIntervalMillis: 150
   },
   acquireConnectionTimeout: 60000
